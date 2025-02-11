458039b4f2e0b0b71cf18787fa899351c2fd6e9f
Your Name <rollercoster246@gmail.com>
2025-02-11 21:32:51 +0530

    Add string handling and concatenation examples in 04_string.py and 05_string_concat.py

---
+++ python/basics/04_string.py
@@ -0,0 +1,23 @@
+# string can either be single or double quotes
+# there is a third way to write strings - 3 single quotes or 3 double quotes
+
+print("Hello")
+print('Hi Boss')
+
+quotes = '''Hello boss
+I think Im in love with you
+What about you'''
+
+
+quotes_2 = """Hello Boss
+I gonna kill you for nothing
+Do u mind, if i perform it"""
+
+print(quotes_2)
+
+first_name = "Sam"
+last_name = "ray"
+
+full_name = first_name +  " " +last_name
+
+print(full_name)
---
+++ python/basics/05_string_concat.py
@@ -0,0 +1,17 @@
+# string concat
+
+print("Sam" + "Ray")
+
+# print("Sam" + 9) # not allowed : TypeError: can only concatenate str (not "int") to str
+
+
+# Solution
+
+print(str(100))
+
+
+# escape sequence
+
+weather = '\t It\'s sunny and i \'love this \' \n climate'
+
+print(weather)
