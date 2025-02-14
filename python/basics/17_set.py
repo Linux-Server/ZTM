013d9d9e6a35446bcf6ddbf19a122bafac690c51
Your Name <rollercoster246@gmail.com>
2025-02-14 16:21:57 +0530

    Add examples of set creation, operations, and actions in 17_set.py and 18_set_action.py

---
+++ python/basics/17_set.py
@@ -0,0 +1,20 @@
+# set - unordered list of unique items
+
+my_set = {1,2,3,4,5}
+
+print(my_set)
+
+print(my_set.add(100))
+
+print(my_set)
+
+
+# exercise , find the unique itesm of the list
+
+my_list = [1,2,3,4,5,5]
+
+print(set(my_list))
+
+# to aceess something from a set
+
+print(1 in my_set)
