cc2c7bf540c5e4a4c28998a5a9ea8da57fa28bbf
Your Name <rollercoster246@gmail.com>
2025-02-14 14:39:27 +0530

    Add examples of None, dictionary creation, and dictionary operations in basics

---
+++ python/basics/13_none.py
@@ -0,0 +1,14 @@
+# None - absence of value
+
+weapon = None
+
+print(weapon)
+
+
+# Dictionary:
+
+my_dict = {"name": "Samray", "age": 22 , "marks": [1,2,3]}
+
+print(my_dict)
+
+print(my_dict["marks"])
