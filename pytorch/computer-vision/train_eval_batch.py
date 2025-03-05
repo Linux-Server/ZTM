38d7e99ec714e1cfc16d07d2b63f0ab7405a4ece
Your Name <rollercoster246@gmail.com>
2025-03-05 11:20:02 +0530

    confusion matrix

--- pytorch/computer-vision/train_eval_batch.py
+++ pytorch/computer-vision/train_eval_batch.py
@@ -37,12 +37,43 @@ def train_step(model: nn.Module,
         optimizer.step()
         
         if batch % 400 == 0:
-            print(f"Look at {batch * len(X)} / {len(train_data) } samples")
+            print(f"Look at {batch * len(X)} / {len(data_loader) * 32 } samples")
     
-    train_loss = train_loss / len(train_data_loader)
-    train_acc = train_acc / len(train_data_loader)
+    train_loss = train_loss / len(data_loader)
+    train_acc = train_acc / len(data_loader)
     
     print(f"Train loss : {train_loss} | Train Acc : {train_acc} ")
+    
+    
+    
+def test_step(model: torch.nn.Module,
+              data_loader : torch.utils.data.DataLoader,
+              loss_fn: nn.Module,
+              accuracy_fn= accuracy_fn,
+              device: torch.device = device):
+    
+    test_loss, test_acc = 0, 0
+    
+    model.eval()        
+
+    with torch.inference_mode():
+
+        for X,y in data_loader:
+            
+            X,y = X.to(device), y.to(device)
+
+            test_logits = model(X)
+            
+            loss = loss_fn(test_logits, y)
+            test_loss += loss
+            
+            test_acc += accuracy_fn(y, test_logits.argmax(dim=1))
+        
+        test_loss = test_loss / len(data_loader)
+        test_acc = test_acc / len(data_loader)
+        
+        print(f"Test loss : {test_loss} |  Test acc : {test_acc}")
+            
     
         
     
